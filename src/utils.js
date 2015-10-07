0352cb5e9759be73df0fdcc311777d53358a2136
Nicholas Dudfield <ndudfield@gmail.com>
2015-10-07 14:42:30 +0700

    Bump version: 0.0.7

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,21 @@
 /* eslint-disable func-style */
 'use strict';
 
+const _ = require('lodash');
+const crypto = require('crypto');
 const {keyPairFromSeed, seedFromPhrase} = require('@niq/ripple-keypairs');
 
-function getKeyPair(secret) {
-  const seed = /^s/.test(secret) ? secret : seedFromPhrase(secret);
-  return keyPairFromSeed(seed);
+function getKeyPair(secret, {type = 'secp256k1'} = {}) {
+  const seed = !secret ? crypto.randomBytes(16) :
+               /^s/.test(secret) ? secret :
+               seedFromPhrase(secret);
+  return keyPairFromSeed(seed, type);
 }
 
 const prettyJSON = obj => JSON.stringify(obj, undefined, 2);
+const memoized = _.memoize(getKeyPair, (...args) => JSON.stringify(args));
 
 module.exports = {
-  getKeyPair,
+  getKeyPair: memoized,
   prettyJSON
 };
